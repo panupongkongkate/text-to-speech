6bde1aa36498c2a768892b61347436be4dda72d1
panupongkongkate <61161511+panupongkongkate@users.noreply.github.com>
2020-04-19 12:59:07 +0700

    test

--- textspees/Form1.Designer.cs
+++ textspees/Form1.Designer.cs
@@ -28,13 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.richTextBox1.Location = new System.Drawing.Point(308, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(566, 245);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button1.Location = new System.Drawing.Point(611, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 121);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "speak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button2.Location = new System.Drawing.Point(308, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 121);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.LargeChange = 8;
+            this.vScrollBar1.Location = new System.Drawing.Point(47, 36);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(80, 379);
+            this.vScrollBar1.TabIndex = 3;
+            this.vScrollBar1.Value = 7;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "เสียง";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "men",
+            "girls"});
+            this.comboBox1.Location = new System.Drawing.Point(145, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "เลือกเพศ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(907, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 97);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Pause";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button4.Location = new System.Drawing.Point(907, 202);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 97);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "resume";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1053, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- textspees/Form1.cs
+++ textspees/Form1.cs
@@ -7,14 +7,121 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using System.Diagnostics;
+
 
 namespace textspees
 {
     public partial class Form1 : Form
     {
+        SpeechSynthesizer synth;
+
+
+        int vAluE = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ประกาซตัวแปร 
+            synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice(); //เซต ให้เป็นออโต้
+            //synth.SelectVoice("Microsoft Server Speech Text to Speech Voice (th-TH, Pattara)");
+            label1.Text = vAluE.ToString();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            vScrollBar1.Enabled = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            synth.Dispose(); //คำสั่งนี้เป็นการทำให้มันค่า ไม่ทำงาน = null
+            if ((richTextBox1.Text != "") && (comboBox1.Text != "")) //แปลว่า ถ้าข้อความไม่ว่างจะไม่สามารถเล่นได้
+            {
+
+                synth = new SpeechSynthesizer();
+                if (comboBox1.Text == "men")
+                {
+                    synth.SelectVoiceByHints(VoiceGender.Male); //เลือกเสียงผู้ชาย
+                    synth.SelectVoice("Microsoft David Desktop"); //ใช้เสียงผู้ชาย
+                }
+                if (comboBox1.Text == "girls")
+                {
+                    synth.SelectVoiceByHints(VoiceGender.Female); //เลือกเสียงผู้หญิง
+                    synth.SelectVoice("Microsoft Zira Desktop"); //ใช้เสียงผู้หญิง
+                }
+                Prompt textsound = new Prompt(richTextBox1.Text);
+                synth.SpeakAsync(textsound);
+                synth.Volume = vAluE;
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                comboBox1.Enabled = false;
+                vScrollBar1.Enabled = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (synth != null) // เซ็ตว่า ถ้า systh ไม่ว่า จะทำงาน
+            {
+                synth.Dispose();
+                button1.Enabled = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                vScrollBar1.Enabled = true;
+                comboBox1.Enabled = true;
+            }
+        }
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            vAluE = vScrollBar1.Value;
+            label1.Text = vAluE.ToString();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //test
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (synth != null)
+            {
+                if (synth.State == SynthesizerState.Speaking) //ถ้าโปรแกรมกำลังอ่าน
+                {
+                    synth.Pause();
+                    button4.Enabled = true;
+                    button3.Enabled = false;
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (synth != null)
+            {
+                if (synth.State == SynthesizerState.Paused) //ถ้าโปรแกรมกำลังพอต
+                {
+                    synth.Resume();
+                    button3.Enabled = true;
+                    button4.Enabled = false;
+                }
+            }
         }
     }
 }
